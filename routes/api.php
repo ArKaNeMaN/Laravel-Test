98d5319bec7b2b06a234911b532813652220ecf2
ArKaNeMaN <ArKaNeMaN1@gmail.com>
2020-10-04 15:35:17 +0300

    2

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Models\Currency as Currency;
+use \App\Models\Coin as Coin;
+use \App\Models\Course as Course;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/v1/coin/{CoinName}/course', function (Request $request, $CoinName) {
+
+    $CurrencyName = $request->currency;
+    if(empty($CurrencyName))
+        return json_encode(['error' => 'Currency is empty']);
+        
+    $Course = Course::join('Coins', 'Courses.CoinId', '=', 'Coins.Id')
+                        ->join('Currency', 'Courses.CurrencyId', '=', 'Currency.Id')
+                        ->where('Currency.ShortName', $CurrencyName)
+                        ->where('Coins.ShortName', $CoinName)
+                        ->select('Courses.Amount', 'Courses.LastUpdate')
+                        ->get()->first();
+
+    if(!$Course)
+        return json_encode(['error' => 'Course not found']);
+
+    return json_encode($Course);
+});
+
+Route::get('/v1/coin/{CoinName}', function (Request $request, $CoinName) {
+
+    $Coin = Coin::where('ShortName', $CoinName)->first();
+    if(!$Coin)
+        return json_encode(['error' => "Coin `{$CoinName}` not found."]);
+
+    return json_encode($Coin);
 });
